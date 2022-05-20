7883c1edbde7e4168e26abb805a9976fbbe7a9bc
Shivam Yadav <sy7052789@gmail.com>
2022-05-20 19:17:55 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv/config");
 const app = express();
 const User = require("./model/user")
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Initial Request!!")

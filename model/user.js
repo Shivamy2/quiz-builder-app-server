da6156c5fb235035c3b15f08fbc7f330f6fd5820
Shivam Yadav <sy7052789@gmail.com>
2022-05-20 19:05:41 +0530

    removed gender attribute and added cors middleware

--- model/user.js
+++ model/user.js
@@ -7,7 +7,6 @@ const UserSchema = new mongoose.Schema({
     first_name: { type: String },
     last_name: { type: String },
     age: { type: Number },
-    gender: { type: String },
     email: { type: String },
     password: { type: String }
 });
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv/config")
+const cors = require("cors");
+require("dotenv/config");
 
 const app = express();
 const User = require("./model/user")
 app.use(express.json());
+app.use(cors);
 
 app.get('/', (req, res) => {
     res.send("Initial Request!!")
